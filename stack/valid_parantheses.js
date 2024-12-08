201ad68cccfb8fa8422ad4ca05075ed713c878cc
Md Ahasan Habib Nahid <ahasanhnahid@gmail.com>
2024-12-08 17:09:37 +0600

    stack valid paranthesis

---
+++ stack/valid_parantheses.js
@@ -0,0 +1,62 @@
+class Stack {
+    constructor() {
+        this.items = []
+    }
+
+    push(element) {
+        this.items.push(element)
+    }
+
+    pop() {
+        if (this.isEmpty()) {
+            return "Stack is Empty"
+        }
+        return this.items.pop()
+    }
+
+    isEmpty() {
+        return this.items.length === 0
+    }
+
+    size() {
+        return this.items.length
+    }
+
+
+}
+
+
+function isValid(s) {
+    const stack = new Stack();
+    const map = {
+        '(':')',
+        '{':'}',
+        '[':']',
+    }
+    for (const char of s) {
+        console.log(map[char]);
+        
+       if (map[char]) {
+        stack.push(char)
+       }
+       else{
+        console.log(char);
+        console.log(map[stack.pop()]);
+        if (stack.isEmpty()|| map[stack.pop()]!==char) {
+           
+            
+            return false
+        }
+       }
+    }
+    return stack.isEmpty();
+}
+
+
+
+// Test cases
+console.log(isValid("()")); // true
+// console.log(isValid("()[]{}")); // true
+// console.log(isValid("(]")); // false
+// console.log(isValid("([])")); // true
+// console.log(isValid("([)]")); // false
